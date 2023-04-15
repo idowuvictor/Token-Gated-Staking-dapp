b4599ace7174f0a2e5a324fc77477a02b896aa8a
Anu <59641189+anupama12sg@users.noreply.github.com>
2023-04-15 19:43:22 +0530

    Improvements in the code-
    - There are a few areas where the code could be optimized which I have done for you:
    a) The `connectCeloWallet()` function could be optimized by removing the nested if statement and directly checking window.celo using a single if statement.
    b) The functions `TotalcEURstaked()`, `TotalcUSDstaked()`, `TotalcRealastaked()`, and `Totalcelostaked()` all have similar structures and can potentially be refactored into a single function to reduce code duplication.
    c)The `notification()` function and the `notificationOff()` function are similar in functionality and could potentially be combined into a single function for better code organization and readability.
    d) The event listener for the `#interestMadeBTN` button could be optimized by extracting the code inside it into a separate function for better code organization and readability.

--- src/main.js
+++ src/main.js
@@ -1,88 +1,82 @@
-import Web3 from "web3"
-import { newKitFromWeb3 } from "@celo/contractkit"
-import BigNumber from "bignumber.js"
-import StakemiiAbi from "../contract/Stakemii.abi.json"
-import erc20Abi from "../contract/erc20.abi.json"
+import Web3 from "web3";
+import { newKitFromWeb3 } from "@celo/contractkit";
+import BigNumber from "bignumber.js";
+import StakemiiAbi from "../contract/Stakemii.abi.json";
+import erc20Abi from "../contract/erc20.abi.json";
 
-const ERC20_DECIMALS = 18
-const StakemiiAddress = "0x00a3C172E6cc85e1681B6148E1a98b77057AFF7A"
-const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
+const ERC20_DECIMALS = 18;
+const StakemiiAddress = "0x00a3C172E6cc85e1681B6148E1a98b77057AFF7A";
+const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
 
-let kit
-let contract
-let products = []
+let kit;
+let contract;
+let products = [];
 
 const connectCeloWallet = async function () {
   if (window.celo) {
-    notification("⚠️ Please approve this DApp to use it.")
+    notification("⚠️ Please approve this DApp to use it.");
     try {
-      await window.celo.enable()
-      notificationOff()
+      await window.celo.enable();
+      notificationOff();
 
-      const web3 = new Web3(window.celo)
-      kit = newKitFromWeb3(web3)
+      const web3 = new Web3(window.celo);
+      kit = newKitFromWeb3(web3);
 
-      const accounts = await kit.web3.eth.getAccounts()
-      kit.defaultAccount = accounts[0]
+      const accounts = await kit.web3.eth.getAccounts();
+      kit.defaultAccount = accounts[0];
 
-      contract = new kit.web3.eth.Contract(StakemiiAbi, StakemiiAddress)
-
-
+      contract = new kit.web3.eth.Contract(StakemiiAbi, StakemiiAddress);
     } catch (error) {
-      notification(`⚠️ ${error}.`)
+      notification(`⚠️ ${error}.`);
     }
   } else {
-    notification("⚠️ Please install the CeloExtensionWallet.")
+    notification("⚠️ Please install the CeloExtensionWallet.");
   }
-}
+};
 
 async function approve(_tokenAddress, _amount) {
-  const cUSDContract = new kit.web3.eth.Contract(erc20Abi, _tokenAddress)
+  const cUSDContract = new kit.web3.eth.Contract(erc20Abi, _tokenAddress);
   const result = await cUSDContract.methods
     .approve(StakemiiAddress, _amount)
-    .send({ from: kit.defaultAccount })
-  return result
+    .send({ from: kit.defaultAccount });
+  return result;
 }
 
 const getBalance = async function () {
-  const totalBalance = await kit.getTotalBalance(kit.defaultAccount)
-  const cUSDBalance = totalBalance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(2)
-  document.querySelector("#balance").textContent = cUSDBalance
-}
+  const totalBalance = await kit.getTotalBalance(kit.defaultAccount);
+  const cUSDBalance = totalBalance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(2);
+  document.querySelector("#balance").textContent = cUSDBalance;
+};
 
 const TotalcEURstaked = async function () {
-  const cEURAddressTotalstaked = await contract.methods.cEURAddressTotalstaked().call()
-  document.querySelector("#TotalcEURstaked").textContent = cEURAddressTotalstaked /1e18
-}
+  const cEURAddressTotalstaked = await contract.methods.cEURAddressTotalstaked().call();
+  document.querySelector("#TotalcEURstaked").textContent = cEURAddressTotalstaked / 1e18;
+};
 
 const TotalcUSDstaked = async function () {
-    const cUSDAddressTotalstaked = await contract.methods.cUSDAddressTotalstaked().call()
-    document.querySelector("#TotalcUSDstaked").textContent = cUSDAddressTotalstaked / 1e18
-  }
+  const cUSDAddressTotalstaked = await contract.methods.cUSDAddressTotalstaked().call();
+  document.querySelector("#TotalcUSDstaked").textContent = cUSDAddressTotalstaked / 1e18;
+};
 
 const TotalcRealastaked = async function () {
-    const cREALAddressTotalstaked = await contract.methods.cREALAddressTotalstaked().call()
-    document.querySelector("#TotalcRealastaked").textContent = cREALAddressTotalstaked /1e18
-}
+  const cREALAddressTotalstaked = await contract.methods.cREALAddressTotalstaked().call();
+  document.querySelector("#TotalcRealastaked").textContent = cREALAddressTotalstaked / 1e18;
+};
 
 const Totalcelostaked = async function () {
-    const CELOAddressTotalstaked = await contract.methods.CELOAddressTotalstaked().call()
-    document.querySelector("#Totalcelostaked").textContent = CELOAddressTotalstaked /1e18
-    console.log("totalBalance", CELOAddressTotalstaked)
-}
+  const CELOAddressTotalstaked = await contract.methods.CELOAddressTotalstaked().call();
+  document.querySelector("#Totalcelostaked").textContent = CELOAddressTotalstaked / 1e18;
+  console.log("totalBalance", CELOAddressTotalstaked);
+};
 
 const totalTransaction = async function () {
-    const totalTransaction = await contract.methods.numberOfStakers().call()
-    document.querySelector("#totalTransaction").textContent = totalTransaction
-    console.log("totalstakers", totalTransaction)
-    }
+  const totalTransaction = await contract.methods.numberOfStakers().call();
+  document.querySelector("#totalTransaction").textContent = totalTransaction;
+  console.log("totalstakers", totalTransaction);
+};
 
 const getAllTokenInvested = async function () {
-    const getAllTokenInvested = await contract.methods.getAllTokenInvested().call()  
-    document.querySelector("#allTokenInvested").textContent = getAllTokenInvested
-    console.log("getAllTokenInvested", getAllTokenInvested)
-    }
-
+  const getAllTokenInvested = await contract.methods.getAllToken
 
 function identiconTemplate(_address) {
   const icon = blockies
